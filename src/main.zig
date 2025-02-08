6b0396762ce0d8828a1b84d50b05197c3892703e
Joakim Lönnegren <joakimlonnegren@gmail.com>
2025-02-08 11:21:29 +0100

    Adds a server struct to keep track of the socket and exit status. Adds commands and command parsing for echo, ping and exit

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,32 @@
 const std = @import("std");
 
+const Commands = enum {
+    exit,
+    ping,
+    echo,
+
+    pub const CommandsTable = [@typeInfo(Commands).Enum.fields.len][:0]const u8{
+        "exit",
+        "ping",
+        "echo",
+    };
+
+    pub fn str(self: Commands) [:0]const u8 {
+        return CommandsTable[std.meta.enumToInt(self)];
+    }
+};
+
+fn parseCommand(str: []const u8) ?Commands {
+    if (std.mem.eql(u8, str, "exit")) {
+        return Commands.exit;
+    } else if (std.mem.eql(u8, str, "ping")) {
+        return Commands.ping;
+    } else if (std.mem.eql(u8, str, "echo")) {
+        return Commands.echo;
+    }
+    return null;
+}
+
 pub fn main() !void {
     var gpa_alloc = std.heap.GeneralPurposeAllocator(.{}){};
     defer std.debug.assert(gpa_alloc.deinit() == .ok);
@@ -7,23 +34,63 @@ pub fn main() !void {
 
     const port = 4657;
     const addr = std.net.Address.initIp4(.{ 0, 0, 0, 0 }, port);
-    var socket = try addr.listen(.{});
+
+    var server = Server{ .exit = false, .address = addr, .tries = std.StringHashMap(Trie).init(gpa) };
+    var socket = try server.address.listen(.{});
 
     std.log.info("Server listening on port {d}", .{port});
 
-    while (true) {
+    while (!server.exit) {
         const client = try socket.accept();
+        defer client.stream.close();
+
         const reader = client.stream.reader();
+        const writer = client.stream.writer();
         while (true) {
             const msg = try reader.readUntilDelimiterOrEofAlloc(gpa, '\n', 65536) orelse break;
             defer gpa.free(msg);
 
-            if (std.mem.eql(u8, msg, "quit")) {
-                std.log.info("client exited", .{});
-                break;
+            var arguments = std.mem.split(u8, msg, " ");
+            const command = arguments.next().?;
+            const parsed = parseCommand(command).?;
+
+            std.log.info("Received command: {s}", .{command});
+            if (msg.len > command.len + 1) {
+                std.log.info("With arguments: {s}", .{msg[command.len + 1 ..]});
             }
 
-            std.log.info("Message: {s}", .{msg});
+            switch (parsed) {
+                .exit => {
+                    std.log.info("Got exit command", .{});
+                    server.exit = true;
+                    break;
+                },
+                .ping => {
+                    std.log.info("Got ping command", .{});
+                    _ = try writer.write("pong\n");
+                },
+                .echo => {
+                    std.log.info("Got echo command", .{});
+                    const echo = try std.fmt.allocPrint(
+                        gpa,
+                        "{s}\n",
+                        .{msg[command.len + 1 ..]},
+                    );
+                    defer gpa.free(echo);
+
+                    _ = try writer.write(echo);
+                },
+            }
         }
     }
 }
+
+const Server = struct {
+    address: std.net.Address,
+    tries: std.StringHashMap(Trie),
+    exit: bool,
+};
+
+const Trie = struct {
+    terminatesWord: bool,
+};
