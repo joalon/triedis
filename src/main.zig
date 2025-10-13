f1b6b53daac3cdaf43b6ceb4b6dc3dad0ce99300
Joakim Lönnegren <joakimlonnegren@gmail.com>
2025-10-13 14:57:45 +0200

    start libxev event loop rewrite. Accept new TCP connections

--- src/main.zig
+++ src/main.zig
@@ -52,109 +52,47 @@ pub fn main() !void {
     const gpa = gpa_alloc.allocator();
 
     const port = 4657;
-    const addr = std.net.Address.initIp4(.{ 0, 0, 0, 0 }, port);
+    const address = std.net.Address.initIp4(.{ 0, 0, 0, 0 }, port);
 
     const tries = try gpa.create(std.StringHashMap(*Trie));
     defer gpa.destroy(tries);
     tries.* = std.StringHashMap(*Trie).init(gpa);
 
-    var server = Server{ .exit = false, .address = addr, .tries = tries };
+    var server = Server{ .exit = false, .address = address, .tries = tries };
 
-    var socket = try server.address.listen(.{});
+    var loop = try xev.Loop.init(.{});
+    defer loop.deinit();
+
+    var tcp_server = try xev.TCP.init(address);
+
+    try tcp_server.bind(address);
+    try tcp_server.listen(128);
 
     std.log.info("Server listening on port {d}", .{port});
 
-    while (!server.exit) {
-        const client = try socket.accept();
-        defer client.stream.close();
+    var acceptCompletion: xev.Completion = .{};
+    tcp_server.accept(&loop, &acceptCompletion, Server, &server, acceptCallback);
 
-        const reader = client.stream.reader();
-        const writer = client.stream.writer();
-        while (true) {
-            const msg = try reader.readUntilDelimiterOrEofAlloc(gpa, '\n', 65536) orelse break;
-            defer gpa.free(msg);
+    try loop.run(.until_done);
+    std.log.info("Event loop done, exiting.", .{});
+}
 
-            var arguments = std.mem.splitScalar(u8, msg, ' ');
-            const command = arguments.next().?;
-            const parsed = parseCommand(command) orelse {
-                _ = try writer.write("invalid command\n");
-                continue;
-            };
+fn acceptCallback(
+    userdata: ?*Server,
+    loop: *xev.Loop,
+    completion: *xev.Completion,
+    result: xev.AcceptError!xev.TCP,
+) xev.CallbackAction {
+    _ = userdata;
+    _ = loop;
+    _ = completion;
+    _ = result catch |err| {
+        std.log.err("Accept client error: {any}.", .{err});
+        return .rearm;
+    };
+    std.log.info("Accepted new client", .{});
 
-            std.log.info("Received command: {s}", .{command});
-            if (msg.len > command.len + 1) {
-                std.log.info("With arguments: {s}", .{msg[command.len + 1 ..]});
-            }
-
-            switch (parsed) {
-                .exit => {
-                    std.log.info("Got exit command", .{});
-                    server.exit = true;
-                    break;
-                },
-                .ping => {
-                    std.log.info("Got ping command", .{});
-                    _ = try writer.write("pong\n");
-                },
-                .echo => {
-                    std.log.info("Got echo command", .{});
-                    const echo = try std.fmt.allocPrint(
-                        gpa,
-                        "{s}\n",
-                        .{msg[command.len + 1 ..]},
-                    );
-                    defer gpa.free(echo);
-
-                    _ = try writer.write(echo);
-                },
-                .create => {
-                    std.log.info("Got create command", .{});
-                    const name = try std.fmt.allocPrint(gpa, "{s}", .{msg[command.len + 1 ..]});
-
-                    const newTrie = try gpa.create(Trie);
-                    newTrie.* = Trie.init(gpa);
-
-                    try server.tries.put(name, newTrie);
-                },
-                .numtries => {
-                    std.log.info("Got numtries command", .{});
-                    const numtries = try std.fmt.allocPrint(
-                        gpa,
-                        "{d}\n",
-                        .{server.tries.count()},
-                    );
-                    defer gpa.free(numtries);
-
-                    _ = try writer.write(numtries);
-                },
-                .insert => {
-                    std.log.info("Got insert command", .{});
-                    const trieName = arguments.next().?;
-                    const insertString = arguments.next().?;
-
-                    var trie = server.tries.get(trieName).?;
-                    try trie.insert(insertString);
-                },
-                .prefixsearch => {
-                    std.log.info("Got prefixsearch command", .{});
-                    const triename = arguments.next().?;
-                    const searchString = arguments.next().?;
-
-                    var trie = server.tries.get(triename).?;
-
-                    var result = std.ArrayList([]const u8).init(gpa);
-                    defer result.deinit();
-
-                    try trie.prefixSearch(&result, searchString);
-
-                    for (result.items) |foundStr| {
-                        _ = try writer.print("{s}\n", .{foundStr});
-                        defer gpa.free(foundStr);
-                    }
-                },
-            }
-        }
-    }
+    return .rearm;
 }
 
 const Server = struct {
