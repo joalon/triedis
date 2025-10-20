782cf8d62cc211caff1a8134216c2c67a277a389
Joakim Lönnegren <joakimlonnegren@gmail.com>
2025-10-20 16:12:12 +0200

    build.zig: add unit tests for trie.zig

--- build.zig
+++ build.zig
@@ -34,14 +34,22 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
+    const trie_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/trie.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
     const exe_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
+    const run_trie_unit_tests = b.addRunArtifact(trie_unit_tests);
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_trie_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
 }
